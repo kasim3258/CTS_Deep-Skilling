3f98173d463da47a370fd3a913ed07048481228c
kasim <kasimvali3258@gmail.com>
2026-06-16 20:50:27 +0530

    Add files via upload

---
+++ React/HOL-1/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "15vh"
+      }}
+    >
+      <h1>Welcome the first session of React</h1>
+    </div>
+  );
+}
+
+export default App;
