8e9425c1567dabe3d89cb0b6e2170151938f07e9
kasim <kasimvali3258@gmail.com>
2026-06-16 11:09:14 +0530

    Add files via upload

---
+++ EF-Core-8.0/Lab-2-Database-Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnConfiguring(
+        DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Server=(localdb)\\MSSQLLocalDB;Database=RetailStoreDb;Trusted_Connection=True;");
+    }
+}
