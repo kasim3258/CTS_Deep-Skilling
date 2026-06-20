4a2a81ee9cd9715e508c00aab169ba72d2009e58
kasim <kasimvali3258@gmail.com>
2026-06-20 09:56:15 +0530

    Add files via upload

---
+++ Design-Principles-Patterns/Exercise-1-Singleton-Pattern/Logger.cs
@@ -0,0 +1,24 @@
+using System;
+
+public sealed class Logger
+{
+    private static Logger instance = null;
+
+    private Logger()
+    {
+    }
+
+    public static Logger GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void Log(string message)
+    {
+        Console.WriteLine("LOG: " + message);
+    }
+}
---
+++ Design-Principles-Patterns/Exercise-1-Singleton-Pattern/program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Logger logger1 = Logger.GetInstance();
+        Logger logger2 = Logger.GetInstance();
+
+        logger1.Log("Application Started");
+        logger2.Log("Application Running");
+
+        if (logger1 == logger2)
+        {
+            Console.WriteLine("Only one Logger instance exists.");
+        }
+        else
+        {
+            Console.WriteLine("Multiple Logger instances exist.");
+        }
+    }
+}
