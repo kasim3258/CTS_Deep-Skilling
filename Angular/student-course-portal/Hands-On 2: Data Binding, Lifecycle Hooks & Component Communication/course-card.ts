be9a89ef758f4ce19480f9d338a8b57df6df1fa7
kasim <kasimvali3258@gmail.com>
2026-07-23 20:51:59 +0530

    Add files via upload

---
+++ Angular/student-course-portal/Hands-On 2: Data Binding, Lifecycle Hooks & Component Communication/course-card.ts
@@ -0,0 +1,36 @@
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  SimpleChanges
+} from '@angular/core';
+
+@Component({
+  selector: 'app-course-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './course-card.html',
+  styleUrl: './course-card.css',
+})
+export class CourseCard implements OnChanges {
+
+  @Input() course!: {
+    id: number;
+    name: string;
+    code: string;
+    credits: number;
+  };
+
+  @Output() enrollRequested = new EventEmitter<number>();
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Course changed:', changes);
+  }
+
+  enroll() {
+    this.enrollRequested.emit(this.course.id);
+  }
+
+}
